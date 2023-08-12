bec12ca147610c340c757cc4d02d9f8339d25c48
vantung2102 <vantung2102@gmail.com>
2023-08-12 10:45:42 +0700

    init

--- src/configs/http.ts
+++ src/configs/http.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import ENV from "constants/env";
 
 const http = axios.create({
-  baseURL: ENV.API_BASE_URL,
+  baseURL: ENV.BASE_URL,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,5 +1,5 @@
 const ENV = {
-  API_BASE_URL: process.env.BASE_URL,
+  BASE_URL: process.env.BASE_URL,
 };
 
 export default ENV;
